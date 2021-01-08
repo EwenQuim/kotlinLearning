34a78076fe91cf9b184397a2533797b844eb1ba1
EwenQuim <ewen.quimerch@gmail.com>
2021-01-08 20:06:48 +0100

    added a button and the action when you click on it

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Clicked !", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+
+    }
+
+    private fun rollDice() {
+        val resultView: TextView = findViewById(R.id.result)
+        val randomInt = Random.nextInt(6) +1
+        resultView.text = randomInt.toString()
     }
 }
